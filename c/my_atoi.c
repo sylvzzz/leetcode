0f87d93a1540db6cb7dd1ad01a1338f33a586084
dbotelho <diogolbsilva2006@gmail.com>
2026-02-26 21:44:24 +0000

    some more for your ahh

--- c/my_atoi.c
+++ c/my_atoi.c
@@ -1,8 +1,10 @@
-int myAtoi(char* s) 
+#include <limits.h>
+
+long myAtoi(char* s) 
 {
     int i = 0;
     int sign = 1;
-    int res = 0;
+    long res = 0;
 
     while (s[i] == ' ')
         i++;
@@ -14,8 +16,20 @@ int myAtoi(char* s)
     }
     while (s[i] >= '0' && s[i] <= '9')
     {
-        res = res * 10 + (s[i] - '0');
+        int digit = s[i] - '0';
+        if (res > (LONG_MAX - digit) / 10)
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+
+        res = res * 10 + digit;
         i++;
     }
-    return (res * sign);
+    res *= sign;
+    if (res > INT_MAX)
+        return INT_MAX;
+    if (res < INT_MIN)
+        return INT_MIN;
+
+    return res;
 }
